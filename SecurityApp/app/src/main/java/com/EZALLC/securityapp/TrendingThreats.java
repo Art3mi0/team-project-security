2a524e186ed68288b1ff43afd8306e9293792bad
Nicholas Morris <nmorris212@gmail.com>
2022-02-16 10:04:48 -0500

    Added activity with textview for TrendingThreats

---
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/TrendingThreats.java
@@ -0,0 +1,14 @@
+package com.EZALLC.securityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrendingThreats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending_threats);
+    }
+}
