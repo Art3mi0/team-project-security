e74d5902b980a99b637509c5bdd6723291b70bb0
iyp4754@uncw.edu <iyp4754@uncw.edu>
2022-02-16 10:39:03 -0500

    created Hello from search component

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/Search.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/Search.java
@@ -3,6 +3,7 @@ package com.EZALLC.securityapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Search extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class Search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) { //done immediately
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        TextView Component = (TextView) findViewById(R.id.Search_Componet);
+        Component.setText(R.string.SearchHello);
+
+
     }
 
 }
