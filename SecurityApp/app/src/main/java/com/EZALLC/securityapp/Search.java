a30c98342fab51db8d0e095059bc6da23b4ec02e
iyp4754@uncw.edu <iyp4754@uncw.edu>
2022-02-16 10:10:11 -0500

    created search class

---
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/Search.java
@@ -0,0 +1,15 @@
+package com.EZALLC.securityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //done immediately
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+}
