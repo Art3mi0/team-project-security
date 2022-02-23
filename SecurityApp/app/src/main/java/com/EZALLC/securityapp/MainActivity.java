a671cf9e60490825449114ff3dd6e058799031e5
Artemio <temomez123@gmail.com>
2022-02-22 22:11:57 -0500

    Added missing activiteis to the xml file

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/MainActivity.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser == null) {
