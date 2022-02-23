27ab5ef7b0ec0a4a6316bcea724c7d9452eea1e8
iyp4754@uncw.edu <iyp4754@uncw.edu>
2022-02-22 21:25:56 -0500

    created Hello from search component created Hello from information component created information component created Search component

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/Information.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/Information.java
@@ -1,21 +1,18 @@
 package com.EZALLC.securityapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class Search extends AppCompatActivity {
-
+public class Information extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) { //done immediately
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_information);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Search");
+        getSupportActionBar().setTitle("Information");
 
     }
-
 }
