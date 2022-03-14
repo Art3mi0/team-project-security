bf240bd69aeadeef04c604a208ca199549a88d2e
Nick <nmorris212@gmail.com>
2022-03-14 18:23:56 -0400

    getThreatLevel() currently causing bug

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/TrendingThreats.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/TrendingThreats.java
@@ -6,10 +6,16 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.EditText;
 
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TrendingThreats extends AppCompatActivity {
+    private EditText threatLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,18 @@ public class TrendingThreats extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        setContentView(R.layout.activity_trending_threats);
+
+        threatLevel = findViewById(R.id.threatLevel);
+//        String stringThreatLevel = getThreatLevel();
+        threatLevel.setText("Threat Level: ");
         getSupportActionBar().setTitle("Trending Threats");
+//        setContentView(R.layout.activity_trending_threats);
+//        TextView threatLevel = findViewById(R.id.textView4);
+//        TextView threatLevel = (TextView) findViewById(R.id.textView4);
+//        threatLevel.setText("Test");
+//
+//        threatLevel.setText(getThreatLevel());
 
     }
     public String getThreatLevel() {
@@ -30,7 +47,24 @@ public class TrendingThreats extends AppCompatActivity {
          * Takes no input and retrieves threat level from IBM X-Force API
          * @return A string of the threat level
          */
-        return "No threat... for now";
+        String content = null;
+        URLConnection connection = null;
+        String threatString = "";
+        try {
+            connection =  new URL("http://www.miltonstart.com/ThreatExplanation.aspx").openConnection();
+            Scanner scanner = new Scanner(connection.getInputStream());
+            scanner.useDelimiter("\\Z");
+            content = scanner.next();
+            scanner.close();
+        }catch ( Exception ex ) {
+            threatString +="Could not retrieve threat level.";
+        }
+
+        String str = "Temp</font></td><td width=\"30\"><font color=\"Black\">";
+        String htmlPage = content;
+        int index1 = htmlPage.indexOf(str)+51;
+        threatString += htmlPage.charAt(index1);
+        return threatString;
     }
     public ArrayList<String> getTopThreats() {
         /**
