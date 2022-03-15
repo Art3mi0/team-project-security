67dc2ade42be7d7a77be521208f8ccfb6692efb6
iyp4754@uncw.edu <iyp4754@uncw.edu>
2022-03-15 01:54:31 -0400

    added text size buttons added function to increase and decrease size.

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/Information_1.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/Information_1.java
@@ -2,13 +2,22 @@ package com.EZALLC.securityapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class Information_1 extends AppCompatActivity {
+    TextView tv;
+    Button IncButton;
+    Button DecButton;
+    private float TextSize = 14f;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_1);
@@ -16,7 +25,23 @@ public class Information_1 extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Information");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        tv =(TextView) findViewById(R.id.textView6);
+        IncButton = (Button) findViewById(R.id.buttonInc);
+        DecButton = (Button) findViewById(R.id.buttonDec);
+        IncButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextSize += 4f;
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, TextSize);
+            }
 
+        });
+        DecButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextSize -= 4f;
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, TextSize);
+            }
+
+        });
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
