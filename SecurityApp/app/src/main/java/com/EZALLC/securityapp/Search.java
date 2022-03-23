9d171f2a134370dc1cff233dcdc7e46988538aee
iyp4754@uncw.edu <iyp4754@uncw.edu>
2022-03-23 00:12:08 -0400

    basic search layout search button connection

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/Search.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/Search.java
@@ -9,52 +9,45 @@ import android.util.TypedValue;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.core.View;
 
 import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
-
+    private Button searchButton;
+    private EditText searchUserInput;
+    private String ipORFile;
+    private ScrollView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) { //done immediately
-
-        Button searchButton;
-        EditText searchUserInput;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Search");
-
-
+        searchView = findViewById(R.id.searchScrollView);
+        searchUserInput= findViewById(R.id.searchInput);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        searchButton = (Button)findViewById(R.id.search_button);
-        searchUserInput  = (EditText) findViewById(R.id.searchInput);
-
-        searchButton.setOnClickListener(
-                new android.view.View.OnClickListener(){
-                    public void onClick(android.view.View v){
-                        //Log.v("EditText", mEdit.getText().toString());
-                    }
-                });
 
 
     }
     /**
-     * This onSearch method receives users file hash or IP address and returns an array with
-     * type and initial input
+     * This onSearch method receives users file hash or IP address
      * and displays on page
      * @param view listener for selection and string with IP or File hash
      * @return arraylist with String IP or FileHash and initial input
      */
-    public ArrayList<String> onSearch(View view, String IpOrFile){
+    public void onSearch(View view, String IpOrFile){
+        ipORFile = searchUserInput.getText().toString();
+        if(ipORFile.isEmpty()){
+            Toast.makeText(Search.this, "Enter a IP address or file Hash", Toast.LENGTH_SHORT).show();
+        }
 
-        ArrayList<String> IpOrFileHash= new ArrayList<String>();
-        return IpOrFileHash;
     }
     /**
      * This onSelectWatchlistIpOrFileHash method retrieves the IP address or File Hash string from the database
