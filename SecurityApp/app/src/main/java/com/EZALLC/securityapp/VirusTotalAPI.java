3913790b75687ec2abd8cd4f94fbd2ad64a1e7f7
TeriyakiSauce69 <lb1603@uncw.edu>
2022-03-27 21:40:51 -0400

    VirusTotal Implementation

---
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/VirusTotalAPI.java
@@ -0,0 +1,19 @@
+package com.EZALLC.securityapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+
+public interface VirusTotalAPI {
+
+    @Headers({"Accept: application/json",
+            "x-apikey: a3824b2a3fe8cf3511178d15fd648cc8fe86c01b14c7ab1cce6ff8578639a3c7"})
+    @GET("ip_addresses/{ip}")
+    Call<IpInfo> getIPInfo(@Path(value = "ip", encoded = true)String Ip);
+
+    @Headers({"Accept: application/json",
+            "x-apikey: a3824b2a3fe8cf3511178d15fd648cc8fe86c01b14c7ab1cce6ff8578639a3c7"})
+    @GET("urls/{url}")
+    Call<HashInfo> getHashInfo(@Path(value = "url", encoded = true)String url);
+}
---
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/VirusTotalClient.java
@@ -0,0 +1,20 @@
+package com.EZALLC.securityapp;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class VirusTotalClient {
+    private static final String BASE_URL = "https://www.virustotal.com/api/v3/";
+
+    private static Retrofit retrofit = null;
+    //.addConverterFactory(GsonConverterFactory.create())
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
