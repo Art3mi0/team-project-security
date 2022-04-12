03dee62563cc92b7720a39d3f4fbb9cfb51ba151
Ixchel Peralta <iyp4754@uncw.edu>
2022-04-11 21:23:41 -0400

    Add a list view for recent search when user leaves the page the searches are cleared or every 10 searches. List view and edittext become Invisible when search. List view visible when user starts typing.

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/Search.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/Search.java
@@ -3,19 +3,21 @@ package com.EZALLC.securityapp;
 import static android.content.ContentValues.TAG;
 
 import android.os.Handler;
-import android.os.SystemClock;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View.OnClickListener;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.google.firebase.firestore.core.View;
 import java.util.ArrayList;
@@ -26,8 +28,11 @@ public class Search extends AppCompatActivity {
     private static long mLastClickTime;
     private Button searchButton;
     private EditText searchUserInput;
-    private String ipOrURL;
+    private String SearchText;
     private ScrollView searchView;
+    private ListView ListViewSearch;
+    public ArrayAdapter<String> SearchAdapter;
+    ArrayList<String> SearchArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) { //done immediately
         super.onCreate(savedInstanceState);
@@ -37,9 +42,36 @@ public class Search extends AppCompatActivity {
         getSupportActionBar().setTitle("Search");
         searchUserInput= findViewById(R.id.searchInput);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /***
+         *
+         */
+        ListViewSearch = findViewById(R.id.SearchList);
+        SearchArray = new ArrayList<>();
+        SearchAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, SearchArray);
+        searchUserInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                ListViewSearch.setVisibility(android.view.View.VISIBLE);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        /***
+         *
+         */
         Button searchButton = (Button) findViewById(R.id.search_button);
         searchButton.setOnClickListener(new android.view.View.OnClickListener(){
             public void onClick(android.view.View v){
+                ListViewSearch.setVisibility(android.view.View.INVISIBLE);
                 if (searchUserInput.getText().toString().isEmpty()) {
                     Toast.makeText(Search.this, "Enter a IP address or URL", Toast.LENGTH_SHORT).show();
                     searchButton.setEnabled(false);
@@ -51,10 +83,10 @@ public class Search extends AppCompatActivity {
                             Log.d(TAG, "resend1");
 
                         }
-                    }, 3000);
+                    }, 2000);
                 }
-                else{
-                    onSearch(searchUserInput.getText().toString());
+                else if (isValidIPAddress(searchUserInput.getText().toString())==false && validURl(searchUserInput.getText().toString())==false) {
+                    Toast.makeText(Search.this, "Enter a IP address or URL", Toast.LENGTH_SHORT).show();
                     searchButton.setEnabled(false);
                     new Handler().postDelayed(new Runnable() {
 
@@ -64,11 +96,45 @@ public class Search extends AppCompatActivity {
                             Log.d(TAG, "resend1");
 
                         }
-                    }, 7000);
+                    }, 2000);
+                }
+                else{
+                    onSearch(searchUserInput.getText().toString());
+                    searchButton.setEnabled(false);
+                    searchUserInput.setEnabled(false);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            searchUserInput.setEnabled(true);
+                            searchButton.setEnabled(true);
+                            Log.d(TAG, "resend1");
+
+                        }
+                    }, 6000);
                     }
 
             }
 
+        });
+        ListViewSearch.setAdapter(SearchAdapter);
+        ListViewSearch.setVisibility(android.view.View.VISIBLE);
+        ListViewSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapterView, android.view.View view, int i, long l) {
+                ListViewSearch.setVisibility(android.view.View.INVISIBLE);
+                onSearch(SearchArray.get(i));
+                searchButton.setEnabled(false);
+                searchUserInput.setEnabled(false);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        searchUserInput.setEnabled(true);
+                        searchButton.setEnabled(true);
+                        Log.d(TAG, "resend1");
+
+                    }
+                }, 6000);
+
+            }
         });
     }
 
@@ -121,16 +187,32 @@ public class Search extends AppCompatActivity {
         }
     }
     /**
+     * Add to vaild ip and urls to front of Search history
+     * removes all searches after ten searches
+     */
+    public void add_clear_list(String ipOrUrlAdd){
+        if(SearchArray.size()<10){
+            SearchArray.add(0,ipOrUrlAdd);
+        }
+        else{
+            SearchArray.clear();
+            SearchArray.add(ipOrUrlAdd);
+        }
+        ListViewSearch.setAdapter(SearchAdapter);
+    }
+    /**
      * This onSearch method receives users file hash or IP address
      * and displays on page
      * @param   ipOrURL
      */
     public void onSearch(String ipOrURL){
             if(isValidIPAddress(ipOrURL)){
+                add_clear_list(ipOrURL);
                 Toast.makeText(Search.this, "Valid Ip Address "+ipOrURL+"\nSearching", Toast.LENGTH_SHORT).show();
 
             }
             else if(validURl(ipOrURL)){
+                add_clear_list(ipOrURL);
                 Toast.makeText(Search.this, "Valid URL "+ipOrURL+"\nSearching", Toast.LENGTH_SHORT).show();
             }
             else {
