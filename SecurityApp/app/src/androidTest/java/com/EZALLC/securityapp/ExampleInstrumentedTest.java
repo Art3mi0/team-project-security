838a46aac22fb1a60428328821fd30f9982678c2
iyp4754@uncw.edu <iyp4754@uncw.edu>
2022-04-29 00:00:47 -0400

    working on the information test toast match

--- SecurityApp/app/src/androidTest/java/com/EZALLC/securityapp/ExampleInstrumentedTest.java
+++ SecurityApp/app/src/androidTest/java/com/EZALLC/securityapp/ExampleInstrumentedTest.java
@@ -1,12 +1,14 @@
 package com.EZALLC.securityapp;
 
+import static android.service.autofill.Validators.not;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -14,9 +16,13 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 import android.content.Context;
+import android.os.IBinder;
 import android.os.SystemClock;
+import android.view.WindowManager;
 
 import androidx.test.espresso.Espresso;
+
+import androidx.test.espresso.Root;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -24,6 +30,8 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -34,6 +42,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 
 /**
@@ -51,24 +60,18 @@ public class ExampleInstrumentedTest {
     }
 }
 
-class InformationTest {
+
+class SearchInformationTest {
     @Rule
     public ActivityScenarioRule<Information> activityActivityTestRule = new ActivityScenarioRule<>(Information.class);
-    public IntentsTestRule<Information> intentsTestRule= new IntentsTestRule<>(Information.class)
+    public IntentsTestRule<Information> intentsTestRule= new IntentsTestRule<>(Information.class);
+    public ActivityTestRule<Information_1> mActivityRule = new ActivityTestRule(Information_1.class);
 
     @Before
     public void setUp() throws Exception{
         Intents.init();
     }
 
-    // Espresso.onView(withId(R.id.textView7)).check(matches(isDisplayed()));
-    //Espresso.onView(withId(R.id.searchInput)).perform(typeText("174.216.16.12"));
-    //Espresso.onView(withId(R.id.searchInput)).check(matches(withText("174.216.16.12")));
-    // Espresso.onView(withId(R.id.search_button)).perform(click());
-    //SystemClock.sleep(1500);
-
-    //        Espresso.onView(withId(R.id.Search)).perform(click());
-//
     @Test
     public void information_Display() {
         Espresso.onView(withId(R.id.ListViewInfo))
@@ -85,29 +88,40 @@ class InformationTest {
                 .check(matches(withText(containsString("https://www.google.com/"))));
         Espresso.onView(withContentDescription("Navigate up")).perform(click());
         intended(hasComponent(Information.class.getName()));
-    }
-    public void fontSizeTest{
-
-    }
-
-        intended(hasComponent(IPHashInfo.class.getName()));
-        SystemClock.sleep(15000);
-        Espresso.onView(withId(R.id.button)).perform(click());
-
-        //openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        //Espresso.onView(withId(android.R.id.home)).perform(click());
-
         Espresso.onView(withContentDescription("Navigate up")).perform(click());
         intended(hasComponent(MainActivity.class.getName()));
+    }
+    public class ToastMatcher extends TypeSafeMatcher<Root> {
+        @Override public void describeTo(Description description) {
+            description.appendText("is toast");
+        }
 
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        Espresso.onView(withText("WatchList")).perform(click());
-        intended(hasComponent(WatchList.class.getName()));
+        @Override public boolean matchesSafely(Root root) {
+            int type = root.getWindowLayoutParams().get().type;
+            if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+                IBinder windowToken = root.getDecorView().getWindowToken();
+                IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                if (windowToken == appToken) {
+                    //means this window isn't contained by any other windows.
+                }
+            }
+            return false;
+        }
+    }
+    public void maxMinTextSizeTest(){
+        Espresso.onView(withId(R.id.buttonInc)).perform(click());
+        Espresso.onView(withId(R.id.buttonInc)).perform(click());
+        Espresso.onView(withId(R.id.buttonInc)).perform(click());
+        Espresso.onView(withId(R.id.buttonInc)).perform(click());
+        Espresso.onView(withId(R.id.buttonInc)).perform(click());
+        Espresso.onView(withText("Maximum text size reached")).inRoot(new ToastMatcher())
+                .check(matches(isDisplayed()));
+        Espresso.onView(withId(R.id.buttonDec)).perform(click());
+        Espresso.onView(withId(R.id.buttonDec)).perform(click());
+        Espresso.onView(withText("Minimum text size reached")).inRoot(new ToastMatcher())
+                .check(matches(isDisplayed()));
+    }
 
-        //Mess with one below.
-        //Espresso.onData(anything()).inAdapterView(withId(R.id.the_watchlist)).atPosition(0).check(matches(isDisplayed()));
-
-        // Espresso.onView(ViewMatchers.withId(R.id.ip_info_page)).perform(ViewActions.click());
 
     @After
     public void tearDown() throws Exception{
