249b9204404488ed0f2d6dde81629014cb097a6f
Art3mi0 <temomez123@gmail.com>
2022-05-01 10:45:00 -0400

    Created recent list, still need to bugfix. Items not ordered, remove not implemented, change what happens on list view item click.

--- SecurityApp/app/src/main/java/com/EZALLC/securityapp/Recent.java
+++ SecurityApp/app/src/main/java/com/EZALLC/securityapp/Recent.java
@@ -2,5 +2,29 @@ package com.EZALLC.securityapp;
 
 public class Recent {
 
+    private String type;
     private String search;
+
+    public Recent(){}
+
+    public Recent(String type, String search) {
+        this.type = type;
+        this.search = search;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
 }
